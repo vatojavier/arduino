80071159ab7c4a999730ea43d86a6972bbafbd94
vatojavier <vatojavier@gmail.com>
2020-05-26 18:10:47 +0200

    mf

--- arduino/motion_follower/motion_follower.ino
+++ arduino/motion_follower/motion_follower.ino
@@ -35,52 +35,61 @@ String leer_string(){
 }
 
 
-void setup() {
-    // put your setup code here, to run once:
-    servo_h.attach(PIN_SERVO_H);
-    servo_v.attach(PIN_SERVO_V);
+/*Map the horizontal and vertical axes and move to the angle*/
+void move_map(String str_h,String str_v){
     
-    servo_h.write(90);
-    servo_v.write(90);
-    
-    Serial.begin(2000000);
-    
-    while(!Serial){;}
-    
-    Serial.setTimeout(READ_TIMEOUT);
-    
-    
-    inString[0] = "";
-    inString[1] = "";
-    
-    delay(500);
-}
-
-/*x-y\n*/
-void loop() {
-
-    while (Serial.available() > 0) {
-    int inChar = Serial.read();
-    if (isDigit(inChar)) {
-      // convert the incoming byte to a char and add it to the string:
-      inString[indexString] += (char)inChar;
-    }
-    // if you get a newline, print the string, then the string's value:
-    if(inChar == '-'){
-        indexString++;
-    }
-    if (inChar == '\n') {
-
-        numero = inString[0].toInt();
+        numero = str_h.toInt();
         angulo_h = map(numero, 20, IMG_WIDTH, MIN_GIRO_H, MAX_GIRO_H);
 
-        numero = inString[1].toInt();
+        numero = str_v.toInt();
         
         angulo_v = map(numero, 20, IMG_WIDTH, MIN_GIRO_V, MAX_GIRO_V);    
 
         
         servo_h.write(angulo_h);
         servo_v.write(angulo_v);
+}
+
+
+/*----- SETUP AND LOOP -----*/
+void setup() {
+        // put your setup code here, to run once:
+        servo_h.attach(PIN_SERVO_H);
+        servo_v.attach(PIN_SERVO_V);
+        
+        servo_h.write(90);
+        servo_v.write(90);
+        
+        Serial.begin(2000000);
+        
+        while(!Serial){;}
+        
+        Serial.setTimeout(READ_TIMEOUT);
+        
+        
+        inString[0] = "";
+        inString[1] = "";
+        
+        delay(500);
+}
+
+/*x-y\n*/
+void loop() {
+    
+    while (Serial.available() > 0) {
+    int inChar = Serial.read();
+    
+    if (isDigit(inChar)) {
+        // convert the incoming byte to a char and add it to the string:
+        inString[indexString] += (char)inChar;
+    }
+    
+    if(inChar == '-'){
+        indexString++;
+    }
+    if (inChar == '\n') {
+
+       move_map(inString[0], inString[1]);
         
         // clear the string for new input:
         inString[0] = "";
